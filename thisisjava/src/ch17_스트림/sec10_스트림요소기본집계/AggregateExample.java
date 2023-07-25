df05b4cc21ffb99fa9ddd42c807d5cfe2028bb62
gus0574 <gus0574@gmail.com>
2023-07-25 18:01:33 +0900

    230725 This is Java

---
+++ thisisjava/src/ch17_스트림/sec10_스트림요소기본집계/AggregateExample.java
@@ -0,0 +1,47 @@
+package ch17_스트림.sec10_스트림요소기본집계;
+
+import java.util.Arrays;
+
+public class AggregateExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		long count = Arrays.stream(arr)
+				.filter(n->n%2==0)
+				.count();
+		System.out.println("2의 배수 개수: "+count);
+		
+		long sum = Arrays.stream(arr)
+				.filter(n->n%2==0)
+				.sum();
+		System.out.println("2의 배수의 합: "+sum);
+		
+		double avg = Arrays.stream(arr)
+				.filter(n->n%2==0)
+				.average()
+				.getAsDouble();
+		System.out.println("2의 배수의 평균: "+avg);
+		
+		int max = Arrays.stream(arr)
+				.filter(n->n%2==0)
+				.max()
+				.getAsInt();
+		System.out.println("최대값: "+max);
+		
+		int min = Arrays.stream(arr)
+				.filter(n->n%2==0)
+				.min()
+				.getAsInt();
+		System.out.println("최소값: "+min);
+		
+		int first = Arrays.stream(arr)
+				.filter(n->n%3==0)
+				.findFirst()
+				.getAsInt();
+		System.out.println("첫 번째 3의 배수: "+first);
+		
+	}
+
+}
